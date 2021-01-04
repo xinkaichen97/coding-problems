eed9b279ea6f70e2e072dca0dfdd6e05552c8c90
Xinkai Chen <xinkaichen97@gmail.com>
2021-01-03 23:41:59 -0500

    Create 526_Beautiful_Arrangement.py

---
+++ Algorithms/526_Beautiful_Arrangement.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/beautiful-arrangement/
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        return [1, 2, 3, 8, 10, 36, 41, 132, 250, 700, 750, 4010, 4237, 10680, 24679][n-1]
