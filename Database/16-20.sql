95d2f7b17c7878a1d17265880502b4896dc15daa
Xinkai Chen <xinkaichen97@gmail.com>
2021-01-04 13:45:53 -0500

    Update 16-20.sql

--- Database/16-20.sql
+++ Database/16-20.sql
@@ -1,4 +1,26 @@
--- Problem 16 (No. 620)
+-- Problem 16 (No. 601)
+-- https://leetcode.com/problems/human-traffic-of-stadium/
+
+SELECT id AS "id", TO_CHAR(visit_date, 'YYYY-MM-DD') AS "visit_date", people AS "people"
+FROM stadium
+WHERE people >= 100
+AND 
+((id + 1 IN (SELECT id FROM stadium WHERE people >= 100) AND id + 2 IN (SELECT id FROM stadium WHERE people >= 100))
+OR (id + 1 IN (SELECT id FROM stadium WHERE people >= 100) AND id - 1 IN (SELECT id FROM stadium WHERE people >= 100))
+OR (id - 1 IN (SELECT id FROM stadium WHERE people >= 100) AND id - 2 IN (SELECT id FROM stadium WHERE people >= 100)));
+                                                                          
+-- Alternative
+SELECT DISTINCT t1.id AS "id", TO_CHAR(t1.visit_date, 'YYYY-MM-DD') AS "visit_date", t1.people AS "people"
+FROM stadium t1, stadium t2, stadium t3
+WHERE t1.people >= 100 AND t2.people >= 100 AND t3.people >= 100
+AND
+((t1.id - t2.id = 1 AND t1.id - t3.id = 2 AND t2.id - t3.id = 1)  -- t1, t2, t3
+OR (t2.id - t1.id = 1 AND t2.id - t3.id = 2 AND t1.id - t3.id = 1) -- t2, t1, t3
+OR (t3.id - t2.id = 1 AND t2.id - t1.id = 1 AND t3.id - t1.id = 2)) -- t3, t2, t1
+ORDER BY t1.id;
+    
+
+-- Problem 17 (No. 620)
 -- https://leetcode.com/problems/not-boring-movies/
 
 SELECT * 
@@ -6,3 +28,18 @@ FROM cinema
 WHERE description <> 'boring'
 AND MOD(id, 2) = 1
 ORDER BY rating DESC
+
+
+-- Problem 18 (No. 626)
+-- https://leetcode.com/problems/exchange-seats/
+SELECT (CASE WHEN MOD(id, 2) != 0 AND seat_count != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND seat_count = id THEN id 
+        ELSE id - 1 END) AS id, student
+FROM seat, (SELECT COUNT(*) AS seat_count FROM seat)
+ORDER BY id;
+
+
+-- Problem 19 (No. 627)
+-- https://leetcode.com/problems/swap-salary/
+UPDATE salary
+SET sex = CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END;
