e9b9647b2056089429ab8aac806c480c053a79a5
Xinkai Chen <xinkaichen97@gmail.com>
2021-01-07 11:48:27 -0500

    Update 16-20.sql

--- Database/16-20.sql
+++ Database/16-20.sql
@@ -43,3 +43,22 @@ ORDER BY id;
 -- https://leetcode.com/problems/swap-salary/
 UPDATE salary
 SET sex = CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END;
+    
+
+-- Problem 20 (No. 1179)
+-- https://leetcode.com/problems/reformat-department-table/
+SELECT id,
+SUM(CASE WHEN month = 'Jan' THEN revenue END) Jan_Revenue,
+SUM(CASE WHEN month = 'Feb' THEN revenue END) Feb_Revenue,
+SUM(CASE WHEN month = 'Mar' THEN revenue END) Mar_Revenue,
+SUM(CASE WHEN month = 'Apr' THEN revenue END) Apr_Revenue,
+SUM(CASE WHEN month = 'May' THEN revenue END) May_Revenue,
+SUM(CASE WHEN month = 'Jun' THEN revenue END) Jun_Revenue,
+SUM(CASE WHEN month = 'Jul' THEN revenue END) Jul_Revenue,
+SUM(CASE WHEN month = 'Aug' THEN revenue END) Aug_Revenue,
+SUM(CASE WHEN month = 'Sep' THEN revenue END) Sep_Revenue,
+SUM(CASE WHEN month = 'Oct' THEN revenue END) Oct_Revenue,
+SUM(CASE WHEN month = 'Nov' THEN revenue END) Nov_Revenue,
+SUM(CASE WHEN month = 'Dec' THEN revenue END) Dec_Revenue
+FROM Department
+GROUP BY id;
