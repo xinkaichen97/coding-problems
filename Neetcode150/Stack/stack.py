f0b0b95f1df0b3b85d3a67fae4a7983edffc9b33
Xinkai Chen <xinkaichen97@gmail.com>
2025-10-14 00:12:41 -0500

    Update stack.py

--- Neetcode150/Stack/stack.py
+++ Neetcode150/Stack/stack.py
@@ -1,8 +1,11 @@
 """
 Problems for stacks
 """
+from typing import List
+
 
 class Solution:
+    
     def isValid(self, s: str) -> bool:
       """
       https://neetcode.io/problems/validate-parentheses
@@ -21,6 +24,24 @@ class Solution:
           stack.append(ch)
       return True if stack else False
 
+
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        """
+        https://neetcode.io/problems/daily-temperatures
+        """
+        stack = []
+        res = []
+        for i, t in enumerate(temperatures):
+            # maintain a monotonic decreasing stack
+            # if current temp is higher, pop from stack and update result
+            while stack and t > stack[-1][0]:
+                stackT, stackI = stack.pop()
+                # index diff
+                res[stackI] = i - stackI
+            # (t, i) as the key since we need the index to update res
+            stack.append((t, i))
+        return res
+        
 
 class MinStack:
     """
