482971dd7ca486530ba536908e4bcf643bde892a
Xinkai Chen <xinkaichen97@gmail.com>
2025-11-16 16:35:43 -0600

    Update dp-1d.py

--- Leetcode75/DP/dp-1d.py
+++ Leetcode75/DP/dp-1d.py
@@ -38,14 +38,35 @@ class Solution:
         n = len(cost)
         dp = [0] * (n + 1)
 
-        # the last value is 0 -> no cost in reaching the end
-        # the second last value is the cost of the last stair
+        # dp[n] == 0 -> no cost in reaching the end
+        # dp[n - 1] is the cost of the last stair
         dp[n - 1] = cost[-1]
 
         # start from the end 
         for i in range(n - 2, -1, -1):
+            # i-th item is the minimum cost starting from i
+            # it's the cost of the i-th stair plus the minimum cost from i+1 or i+2
             dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
 
         # return the minimum of first and second items
         return min(dp[0], dp[1])
-      
+
+
+    def minCostClimbingStairs2(self, cost: List[int]) -> int:
+        """
+        https://leetcode.com/problems/min-cost-climbing-stairs
+        Time: O(N), Space: O(N)
+        """
+        # initialize DP array
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        # start from the beginning
+        # dp[0] & dp[1] == 0 -> no cost to start from 0 or 1
+        for i in range(2, n + 1):
+            # i-th item is the minimum cost to reach i
+            # it's the minimum cost of i-1 plus cost[i-1] or i-2 plus cost[i-2]
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+          
+        # return the last item 
+        return dp[n]
