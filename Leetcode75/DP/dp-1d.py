8a078841ea8d91ac7b18d28322d1360520f17254
Xinkai Chen <xinkaichen97@gmail.com>
2025-11-17 23:58:27 -0600

    Update dp-1d.py

--- Leetcode75/DP/dp-1d.py
+++ Leetcode75/DP/dp-1d.py
@@ -70,3 +70,24 @@ class Solution:
           
         # return the last item 
         return dp[n]
+
+
+    def rob(self, nums: List[int]) -> int:
+        """
+        https://leetcode.com/problems/house-robber
+        Time: O(N), Space: O(N)
+        """
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * n
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        
+        return dp[-1] 
