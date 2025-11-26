8c0f56f873de41b66884c63b75a63d54e48001f5
Xinkai Chen <xinkaichen97@gmail.com>
2025-11-26 16:30:45 -0600

    Update binary-search.py

--- Leetcode75/Binary-Search/binary-search.py
+++ Leetcode75/Binary-Search/binary-search.py
@@ -81,3 +81,37 @@ class Solution:
             res.append(m - idx if idx >= 0 else 0)
 
         return res
+
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        https://leetcode.com/problems/koko-eating-bananas
+        Time: (nlogm), Space: O(1)
+        n -> len(piles), m -> max(piles)
+        """
+        # helper function to see if Koko can finish at k
+        def canFinish(k: int) -> bool:
+            t = 0
+            for p in piles:
+                # if p is not divisible by k, add one (math.ceil)
+                t += p // k
+                if p % k:
+                    t += 1
+                if t > h:
+                    return False
+            return True
+
+        # binary search from 1 to the max pile
+        l, r = 1, max(piles)
+        res = r
+        while l <= r:
+            mid = (l + r) // 2
+            # if can finish, search in the left and update the answer
+            if canFinish(mid):
+                res = mid
+                r = mid - 1
+            # otherwise search in the right
+            else:
+                l = mid + 1
+                
+        return res
