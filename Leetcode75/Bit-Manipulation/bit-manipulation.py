756e7bc0d620652173bf6d3563c1cf9f9df7adc6
Xinkai Chen <xinkaichen97@gmail.com>
2025-12-11 23:37:19 -0600

    Update bit-manipulation.py

--- Leetcode75/Bit-Manipulation/bit-manipulation.py
+++ Leetcode75/Bit-Manipulation/bit-manipulation.py
@@ -4,7 +4,7 @@ Problems for Bit Manipulation
 from typing import List
 
 
-class Solution(object):
+class Solution:
   
     def countBits(self, n: int) -> List[int]:
         """
@@ -19,3 +19,42 @@ class Solution(object):
             res[x] = res[x >> 1] + (x & 1) 
 
         return res 
+
+
+    def singleNumber(self, nums: List[int]) -> int:
+        """
+        https://leetcode.com/problems/single-number
+        Time: O(n), Space: O(1)
+        """
+        res = 0
+        
+        # perform XOR on all numbers, the rest is the single one
+        for i in nums:
+            res ^= i
+          
+        return res
+
+
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        """
+        https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c
+        Time: O(n), Space: O(1)
+        """
+        res = 0
+      
+        # calculate until all three become zero
+        while a or b or c:
+            # if c's last bit is 1, only flip if a or b last bits are both 0's
+            if c & 1:
+                res += 0 if ((a & 1) or (b & 1)) else 1
+            # if c's last bit is 0, a and b last bits must both be zero
+            else:
+                res += (a & 1) + (b & 1)
+            
+            # shift all numbers to the right to check the next bits
+            a >>= 1
+            b >>= 1
+            c >>= 1
+            
+        return res
+      
