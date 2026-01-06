157568afa54844e0ddc9db05cda61e008021e5f2
Xinkai Chen <xinkaichen97@gmail.com>
2026-01-06 17:12:36 -0600

    Update bfs.py

--- Leetcode75/Graphs/bfs.py
+++ Leetcode75/Graphs/bfs.py
@@ -6,6 +6,45 @@ from typing import List
 
 class Solution:
 
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        """
+        https://leetcode.com/problems/nearest-exit-from-entrance-in-maze
+        Time: O(m * n), Space: O(m + n)
+        """
+        rows, cols = len(maze), len(maze[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        # mark entrance as a wall because it's not an exit
+        start_row, start_col = entrance
+        maze[start_row][start_col] = "+"
+
+        # define queue and add the entrance point
+        q = collections.deque()
+        q.append([start_row, start_col, 0])
+
+        # maintain the queue for BFS
+        while q:
+            # get current location and distance
+            curr_row, curr_col, curr_distance = q.popleft()
+
+            # check all four directions
+            for dr, dc in directions:
+                next_row = curr_row + dr
+                next_col = curr_col + dc
+
+                # if the move is valid and there's an empty cell
+                if 0 <= next_row < rows and 0 <= next_col < cols and maze[next_row][next_col] == ".":
+                    # if at the border, we found the exit, return the distance traveled
+                    if 0 == next_row or next_row == rows - 1 or 0 == next_col or next_col == cols - 1:
+                        return curr_distance + 1
+
+                    # mark the next move as visited
+                    maze[next_row][next_col] = "+"
+                    q.append([next_row, next_col, curr_distance + 1])
+        
+        return -1
+
+    
     def orangesRotting(self, grid: List[List[int]]) -> int:
         """
         https://leetcode.com/problems/rotting-oranges
