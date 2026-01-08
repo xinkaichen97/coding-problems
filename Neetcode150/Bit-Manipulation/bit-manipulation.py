36fb623c3150987de8d3debeda5cd3275be92bcf
Xinkai Chen <xinkaichen97@gmail.com>
2026-01-08 16:38:48 -0600

    Update bit-manipulation.py

--- Neetcode150/Bit-Manipulation/bit-manipulation.py
+++ Neetcode150/Bit-Manipulation/bit-manipulation.py
@@ -8,7 +8,7 @@ class Solution:
 
     def hammingWeight(self, n: int) -> int:
         """
-        https://neetcode.io/problems/counting-bits/
+        https://neetcode.io/problems/number-of-one-bits
         Time: O(1), Space: O(1)
         """
         res = 0
@@ -26,7 +26,7 @@ class Solution:
   
     def countBits(self, n: int) -> List[int]:
         """
-        https://neetcode.io/problems/counting-bits/
+        https://neetcode.io/problems/counting-bits
         Time: O(n), Space: O(1)
         """
         offset = 1
@@ -41,4 +41,64 @@ class Solution:
             dp[i] = 1 + dp[i - offset]
           
         return dp
-      
+
+
+    def reverseBits(self, n: int) -> int:
+        """
+        https://neetcode.io/problems/reverse-bits
+        Time: O(1), Space: O(1)
+        """
+        res = 0
+        for i in range(32):
+            # get the i-th bit of n
+            bit = (n >> i) & 1
+            # put it at position (31 - i), use OR to avoid affecting other bits
+            res = res | (bit << (31 - i))
+            
+        return res
+
+
+    def missingNumber(self, nums: List[int]) -> int:
+        """
+        https://neetcode.io/problems/missing-number
+        Time: O(n), Space: O(1)
+        """
+        res = len(nums)
+        for i in range(len(nums)):
+            # for each index and number, do XOR until there's one number left
+            res ^= (i ^ nums[i])
+        return res
+
+
+    def getSum(self, a: int, b: int) -> int:
+        """
+        https://neetcode.io/problems/sum-of-two-integers
+        Time: O(1), Space: O(1)
+        """
+        res = 0
+        carry = 0
+        mask = 0xFFFFFFFF # all 1's
+        max_int = 0x7FFFFFFF # max positive int, 011...11
+
+        for i in range(32):
+            # get i-th bits from a and b
+            a_bit = (a >> i) & 1
+            b_bit = (b >> i) & 1
+            # use XOR to get the current bit after carry
+            bit = a_bit ^ b_bit ^ carry
+            # calculate if there's a carry
+            carry = (a_bit + b_bit + carry) >= 2
+            # add the current bit to res
+            if bit:
+                res |= (1 << i)
+
+        # handle negative numbers in Python
+        if res > max_int:
+            # use mask to flip all bits and get (magnitude - 1)
+            # then do a NOT operation to get the correct negative number
+            res = ~(res ^ mask)
+            ## equivalent: minus 2^32
+            # res = res - 0x100000000
+        
+        return res
+        
