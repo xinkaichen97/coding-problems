03b119509370b9618dca6dafd4803eeeac809203
Xinkai Chen <xinkaichen97@gmail.com>
2026-01-13 23:24:28 -0600

    Update dp-1d.py

--- Neetcode150/DP/dp-1d.py
+++ Neetcode150/DP/dp-1d.py
@@ -154,3 +154,28 @@ class Solution:
                     res += 1
     
         return res
+
+
+    def numDecodings(self, s: str) -> int:
+        """
+        https://neetcode.io/problems/decode-ways
+        Time: O(n), Space: O(n)
+        """
+        # initialize the dp dict
+        dp = {len(s): 1}
+
+        # bottom-up
+        for i in range(len(s) - 1, -1, -1):
+            # base case: invalid decoding
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+            # if i + 1 is in bounds, and the two letters are between 10 and 26
+            # add two-letter decoding
+            if i + 1 < len(s) and (s[i] == "1" or 
+                s[i] == "2" and s[i + 1] in "0123456"):
+                dp[i] += dp[i + 2]
+        
+        return dp[0]
+      
