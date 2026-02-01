7a1fa1d144d0f8b8c09f70a4f9593b4e0890dcd6
Xinkai Chen <xinkaichen97@gmail.com>
2026-01-31 19:44:12 -0600

    Update dp-2d.py

--- Neetcode150/DP/dp-2d.py
+++ Neetcode150/DP/dp-2d.py
@@ -11,7 +11,7 @@ class Solution:
         https://neetcode.io/problems/count-paths
         Time: O(m * n), Space: O(m * n) - can be optimized to O(n)
         """
-        # dp[i][j] - the number of unique paths to index i,j
+        # dp[i][j] - the number of unique paths to index (i, j)
         # initialize as 1 for the first row and column
         dp = [[1] * n for _ in range(m)]
         
@@ -30,3 +30,25 @@ class Solution:
         # return dp[n - 1]
         
         return dp[m - 1][n - 1]
+
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        """
+        https://neetcode.io/problems/count-paths
+        Time: O(m * n), Space: O(m * n) - can be optimized to O(n)
+        """
+        # dp[i][j] - LCS from index (i, j)
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+
+        # go from bottom-right to top-left
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                # if the character is the same, the result is LCS(i+1, j+1) + 1
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    # if not, the result is the max of advancing one char in either text
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+                    
+        return dp[0][0]
+      
