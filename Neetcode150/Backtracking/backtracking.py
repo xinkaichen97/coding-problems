82e21a90270edc6c098730a209cfc86a6819e90b
Xinkai Chen <xinkaichen97@gmail.com>
2026-02-15 11:50:45 -0600

    Update backtracking.py

--- Neetcode150/Backtracking/backtracking.py
+++ Neetcode150/Backtracking/backtracking.py
@@ -258,4 +258,43 @@ class Solution:
         
         backtrack(0)
         return res
+
+    
+    def letterCombinations(self, digits: str) -> List[str]:
+        """
+        https://neetcode.io/problems/combinations-of-a-phone-number
+        Time: O(n * 4^n), Space: O(n)
+        """
+        if not digits:
+            return []
+          
+        # number to letter mapping
+        mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        res = []
+
+        def backtrack(idx, comb):
+            # return condition: indices reach the end
+            if idx == len(digits):
+                res.append(comb)
+                return
+
+            # call backtrack with index + 1 and updated letters
+            # for each letter in digits, the worst case is 4 possible next letters, so the total path is 4^n
+            # and since string concatenation is O(n), total time complexity is O(n * 4^n)
+            for letter in mapping[digits[idx]]:
+                backtrack(idx + 1, comb + letter)
+
+        # backtrack from index 0 and an empty string
+        backtrack(0, "")
+
+        return res
         
