0892133f7f949f8748aad24502b783208b7224e9
Xinkai Chen <xinkaichen97@gmail.com>
2026-02-16 08:27:32 -0600

    Update graphs.py

--- Neetcode150/Graphs/graphs.py
+++ Neetcode150/Graphs/graphs.py
@@ -45,6 +45,31 @@ class Solution:
         return res
 
 
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """
+        https://neetcode.io/problems/max-area-of-island
+        Time: O(m * n), Space: O(m * n)
+        """
+        m, n = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            # if out of bounds or not an island or already visited
+            if r < 0 or c < 0 or r >= m or c >= n or grid[r][c] == 0:
+                return 0
+            # mark as visited
+            grid[r][c] = 0
+            # sum all directions and add 1
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+        # go through every grid and get max 
+        res = 0
+        for r in range(m):
+            for c in range(n):
+                res = max(res, dfs(r, c))
+        
+        return res
+
+    
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         """
         https://neetcode.io/problems/clone-graph
