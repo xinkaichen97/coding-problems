42f0dafa3a535a8c779f2819ade2763ec765417d
Xinkai Chen <xinkaichen97@gmail.com>
2026-02-19 22:30:42 -0600

    Update intervals.py

--- Neetcode150/Intervals/intervals.py
+++ Neetcode150/Intervals/intervals.py
@@ -97,7 +97,43 @@ class Solution:
         #         merged = []
         # return res
         
-        
+
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        """
+        https://neetcode.io/problems/non-overlapping-intervals
+        Time: O(nlogn), Space: O(n)
+        """
+        # sort by end times
+        intervals.sort(key = lambda x: x[1])
+
+        # keep track of the previous end
+        prevEnd = intervals[0][1]
+        res = 0
+
+        for i in range(1, len(intervals)):
+            # if prev end is greater than start, there's an overlap
+            # already sorted by end times so the current end must be greater, we remove the current interval
+            if prevEnd > intervals[i][0]:
+                res += 1
+            # if no overlap, update the previous end with the current end
+            else:
+                prevEnd = intervals[i][1]
+
+        return res
+
+        # # sort by start times
+        # intervals.sort()
+        # prevEnd = intervals[0][1]
+        # res = 0
+        # for i in range(1, len(intervals)):
+        #     if prevEnd > intervals[i][0]:
+        #         prevEnd = min(prevEnd, intervals[i][1])
+        #         res += 1
+        #     else:
+        #         prevEnd = intervals[i][1]
+        # return res
+
+    
     def canAttendMeetings(self, intervals: List[Interval]) -> bool:
         """
         https://neetcode.io/problems/meeting-schedule
@@ -144,27 +180,15 @@ class Solution:
             res = max(res, count)
         return res
 
-    
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        """
-        https://neetcode.io/problems/non-overlapping-intervals
-        Time: O(nlogn), Space: O(n)
-        """
-        # sort by end times
-        intervals.sort(key = lambda x: x[1])
-
-        # keep track of the previous end
-        prevEnd = intervals[0][1]
-        res = 0
-
-        for i in range(1, len(intervals)):
-            # if prev end is greater than start, there's an overlap
-            # already sorted by end times so the current end must be greater, we remove the current interval
-            if prevEnd > intervals[i][0]:
-                res += 1
-            # if no overlap, update the previous end with the current end
-            else:
-                prevEnd = intervals[i][1]
-
-        return res
+        # # sweep line algorithm
+        # mp = defaultdict(int)
+        # for i in intervals:
+        #     mp[i.start] += 1
+        #     mp[i.end] -= 1
+        # res = 0
+        # curr = 0
+        # for i in sorted(mp):
+        #     curr += mp[i]
+        #     res = max(res, curr)
+        # return res
       
