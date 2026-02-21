b36ec30d5a37186c9160d9052dc24a61344c6b37
Xinkai Chen <xinkaichen97@gmail.com>
2026-02-20 23:30:52 -0600

    Update greedy.py

--- Neetcode150/Greedy/greedy.py
+++ Neetcode150/Greedy/greedy.py
@@ -1,6 +1,7 @@
 """
 Problems for Greedy
 """
+from typing import List
 
 
 class Solution:
@@ -60,3 +61,34 @@ class Solution:
                 goal = i
         return goal == 0
       
+
+    def jump(self, nums: List[int]) -> int:
+        """
+        https://neetcode.io/problems/jump-game-ii
+        Time: O(n), Space: O(1)
+        """
+        # # DP - O(n^2)
+        # dp = [float("inf")] * len(nums)
+        # dp[-1] = 0
+        # for i in range(len(nums) - 2, -1, -1):
+        #     end = min(len(nums), i + nums[i] + 1)
+        #     for j in range(i + 1, end):
+        #         dp[i] = min(dp[i], 1 + dp[j])
+        # return dp[0]
+        
+        res = 0
+        l, r = 0, 0
+        while r < len(nums) - 1:
+            # the farthest it can jump from the current range [l, r]
+            nxt = r
+            while l <= r:
+                nxt = max(nxt, l + nums[l])
+                l += 1
+            # go to the next range [r + 1, nxt]
+            l = r + 1
+            r = nxt
+            # increment res as we take greedy steps every time
+            res += 1
+            
+        return res
+        
