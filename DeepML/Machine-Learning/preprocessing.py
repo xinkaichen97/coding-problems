60bab57cbe3fa7fc899190672ab5aa2c5544c0a4
Xinkai Chen <xinkaichen97@gmail.com>
2026-04-17 00:06:34 -0500

    Add ordinal label encoding function
    Added a function to encode ordinal categorical values to integers based on a specified order.

--- DeepML/Machine-Learning/preprocessing.py
+++ DeepML/Machine-Learning/preprocessing.py
@@ -16,4 +16,19 @@ def convert_range(values: np.ndarray, c: float, d: float) -> np.ndarray:
         return np.full_like(x, c, dtype=float)
     x = c + (d - c) / (b - a) * (x - a)
     return x
-  
+
+
+def label_encode_ordinal(values: list, order: list) -> list:
+    """
+    356. Encode ordinal categorical values to integers based on specified order.
+    https://www.deep-ml.com/problems/356
+    
+    Args:
+        values: List of categorical values to encode
+        order: List specifying the order of categories from lowest (0) to highest
+    Returns:
+        List of integers representing the encoded values
+    """
+    mp = {cat: i for i, cat in enumerate(order)}
+    return [mp.get(val, -1) for val in values]
+    
